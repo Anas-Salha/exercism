dbdd284ef68b5864827cb89a0a98ea50f9e67046
Anas Salha <anassalha96@gmail.com>
2023-11-04 19:54:24 +0100

    Updated Rust gigasecond exercise to use the Add trait instead of saturating add

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,7 +1,7 @@
-use time::ext::NumericalDuration;
+use time::Duration;
 use time::PrimitiveDateTime as DateTime;
 
 // Returns a DateTime one billion seconds after start.
 pub fn after(start: DateTime) -> DateTime {
-    start.saturating_add(1000000000.seconds())
+    start + Duration::seconds(1_000_000_000)
 }
