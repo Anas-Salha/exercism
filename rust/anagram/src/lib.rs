0a69d26adec73863fe70c526200b63e455b2be9c
Anas-Salha <anassalha96@gmail.com>
2023-11-15 22:32:41 +0100

    Initial solution to Rust anagram

--- rust/anagram/src/lib.rs
+++ rust/anagram/src/lib.rs
@@ -1,5 +1,23 @@
 use std::collections::HashSet;
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
-    todo!("For the '{word}' word find anagrams among the following words: {possible_anagrams:?}");
+pub fn anagrams_for<'a>(word: &str, inputs: &[&'a str]) -> HashSet<&'a str> {
+    let word_lowercase = word.to_lowercase();
+    let word_sorted = sort_str(&word_lowercase);
+
+    inputs
+        .iter()
+        .filter(|&input|{
+            input.len() == word.len() &&
+            word_lowercase != input.to_lowercase() &&
+            word_sorted == sort_str(&input.to_lowercase())
+        })
+        .cloned()
+        .collect()
+
+}
+
+pub fn sort_str(word: &str) -> Vec<char> {
+    let mut chars: Vec<char> = word.chars().collect();
+    chars.sort_unstable();
+    chars
 }
